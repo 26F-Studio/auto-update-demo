dec280c55a7e29c36274c2b9e01ad622d6105da0
Imple Lee <80144331+ImpleLee@users.noreply.github.com>
2023-03-12 21:14:40 +0800

    use in-memory zip file

--- server/app.py
+++ server/app.py
@@ -2,12 +2,40 @@ from flask import Flask, abort, send_file
 from pathlib import Path
 from zipfile import ZipFile
 from filecmp import cmp
-from io import StringIO
+from io import StringIO, BytesIO
 from uuid import uuid4
+from functools import cache
 
 app = Flask(__name__)
 
 UUID = uuid4().hex
+
+@cache
+def zip_for(version):
+  dir = Path('versions') / version
+  latest = Path('versions') / 'latest'
+  data = BytesIO()
+  with ZipFile(data, 'w') as zip:
+    for file in latest.iterdir():
+      if not file.is_file():
+        continue
+      relative_path = file.relative_to(latest)
+      old_version = dir / relative_path
+      if old_version.exists() and cmp(file, old_version, shallow=False):
+        continue
+      zip.write(file, Path('code') / relative_path)
+    with StringIO() as f:
+      written = False
+      for file in dir.iterdir():
+        if not file.is_file():
+          continue
+        relative_path = file.relative_to(dir)
+        if not (latest / relative_path).exists():
+          print(relative_path, file=f)
+          written = True
+      if written:
+        zip.writestr('removed_file.txt', f.getvalue())
+  return data
 
 @app.route('/diff/<string:version>')
 def generate_diff(version):
@@ -18,29 +46,8 @@ def generate_diff(version):
   check_next()
   if latest.resolve() == dir.resolve():
     abort(404)
-  data = Path('zips') / f'{version}.zip'
-  if not data.exists():
-    data.parent.mkdir(parents=True, exist_ok=True)
-    with ZipFile(data, 'w') as zip:
-      for file in latest.iterdir():
-        if not file.is_file():
-          continue
-        relative_path = file.relative_to(latest)
-        old_version = dir / relative_path
-        if old_version.exists() and cmp(file, old_version, shallow=False):
-          continue
-        zip.write(file, Path('code') / relative_path)
-      with StringIO() as f:
-        written = False
-        for file in dir.iterdir():
-          if not file.is_file():
-            continue
-          relative_path = file.relative_to(dir)
-          if not (latest / relative_path).exists():
-            print(relative_path, file=f)
-            written = True
-        if written:
-          zip.writestr('removed_file.txt', f.getvalue())
+  data = zip_for(version)
+  data.seek(0)
   return send_file(data, download_name=f'{UUID}-{version}.zip')
 
 def check_next():
@@ -62,10 +69,7 @@ def set_latest(version):
   if file.exists():
     file.unlink()
   file.symlink_to(version)
-  zip_dir = Path('zips')
-  if zip_dir.exists():
-    for file in zip_dir.iterdir():
-      file.unlink()
+  zip_for.cache_clear()
 
 if __name__ == '__main__':
   app.run(debug=True)
